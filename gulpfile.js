933193716d62b3be74e543d2a704d60fd54c9758
Aion Chiu <luffy110661@gmail.com>
2021-01-23 18:13:38 +0800

    feat(css): gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,42 @@
-﻿
-const { src, dest, watch } = require('gulp');
+﻿const { src, dest, watch } = require('gulp');
 const rename = require('gulp-rename');
 const babel = require('gulp-babel');
 const through = require('through');
 const sourcemaps = require('gulp-sourcemaps');
 const lazypipe = require('lazypipe');
 const cached = require('gulp-cached');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+sass.compiler = require('node-sass');
 
+// js
 const MUST_EXCLUDE = ['!**/*.min.js', '!gulpfile.js', '!node_modules/**'];
 const EXPORTS_FILE = (option = {}) => ['src/js/**/*.js'].concat(MUST_EXCLUDE, option.moreFiles || []);
 const OPTION = { base: 'src', removeBOM: false, allowEmpty: true };
 
+// sass
+const SASS_FILE = ['src/sass/style-edit.scss'];
+
+const postcssPlugin = [
+  autoprefixer(),
+]
+
+function compilerSass() {
+  return src(SASS_FILE)
+    .pipe(cached('css'))
+    .pipe(renameFile())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(postcss(postcssPlugin))
+    .pipe(dest('./dist/css'))
+}
+
 const renameFile = () => lazypipe()
-  .pipe(rename, path => path.basename += '.min')()
+  .pipe(rename, path => {
+    const isSass = /.s[ac]ss$/i.test(path.extname);
+    isSass ? path.basename = 'style-source' : path.basename += '.min';
+  } 
+  )()
 
 const optimizeJs = (option = {}) => lazypipe()
   .pipe(option.production ? through : sourcemaps.init)
@@ -36,11 +60,12 @@ async function minJs() {
     .on('error', err => {
       console.log(err)
     })
-    .pipe(dest('./dist'))
+    .pipe(dest('./dist/js'))
 }
 
 module.exports = Object.assign({},
   {
     'watch:js': watchJs,
-  }
+    'min:css': compilerSass,
+  },
 )
