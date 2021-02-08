160c0992816d99f7be4073707a7bc108d0fea163
Aion Chiu <aion.chiu@isobar.com>
2021-02-08 19:01:41 +0800

    feat(task): 產生 .config

---
+++ task/init.js
@@ -0,0 +1,11 @@
+﻿const { hasPath } = require('./util');
+const generateConfig = require('./generate-config');
+const generateDirectory = require('./generate-directory');
+const { join, posix, resolve } = require('path');
+const configPath = resolve(__dirname, '.config');
+
+if (!hasPath(configPath)) {
+  generateConfig();
+} else {
+  generateDirectory();
+}
---
+++ task/util.js
@@ -0,0 +1,21 @@
+﻿
+const { existsSync } = require('fs');
+const { join, posix } = require('path');
+
+function validPath(path) {
+  const matchSlash = path.match('/');
+  if (matchSlash === null || matchSlash.index <= 0) {
+    return null;
+  }
+  return path;
+}
+
+function hasPath(path) {
+  const existence = existsSync(path);
+  return existence;
+}
+
+module.exports = {
+  hasPath,
+  validPath,
+}
