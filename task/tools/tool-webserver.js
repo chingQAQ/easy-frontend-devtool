5ccaa19c61a9a74e64bb5e56ea4e093afcce3668
Aion Chiu <luffy110661@gmail.com>
2021-02-17 04:12:38 +0800

    feat: webserver
    - frontend develop server
    - 靜態路徑
    - proxy 

--- task/tools/tool-webserver.js
+++ task/tools/tool-webserver.js
@@ -3,21 +3,40 @@ const webserver = require('gulp-webserver');
 const PATH = require('./config');
 const viewPath = PATH.source.split(process.cwd())[1];
 const option = { removeBOM: false, allowEmpty: true };
-const serveStatic = require('serve-static')
+const serveStatic = require('serve-static');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { root: proxyRoot, option: proxyOptions } = {
+  root: '/api',
+  option: {
+    target: 'https://www.example.com/',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api': '',
+    },
+    router: {
+      'dev.localhost:7000': 'http://localhost:7000',
+    },
+  }
+};
+const middleware = {
+  static: serveStatic(PATH.source),
+  proxy: createProxyMiddleware(proxyRoot, proxyOptions) // proxy想不到要用在哪先放上來
+};
+
 const stream = {
   webserver: () => webserver({
     path: viewPath,
     livereload: true,
-    directoryListing: true,
+    directoryListing: false,
     open: true,
     fallback: 'index.html',
-    middleware: [serveStatic(PATH.source)]
+    middleware: [middleware.static]
   })
-}
+};
 
 function webServer() {
   return src(PATH.source, Object.assign({}, option))
     .pipe(stream.webserver())
-}
+};
 
 module.exports = webServer;
