ddaf63b3954dc33d5d41d5da306c9c8563f1e7c9
Aion Chiu <aion.chiu@isobar.com>
2021-02-19 16:20:41 +0800

    update

--- asset/js/index.js
+++
@@ -1,9 +0,0 @@
-﻿const { coin, discount } = object => {
-  const type = typeof object === number;
-  const x = type ? object * 10 : 0;
-  const y = type ? 0.7 : 0;
-  return {
-    coin: x,
-    discount: y 
-  }
-}
